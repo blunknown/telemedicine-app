5bd83575404e14a25a37ac12feef5a9409bc35c2
Paul Fernando <paulfernando128@gmail.com>
2020-12-01 20:12:46 -0500

    guards updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard';
+import { RoleGuard } from './core/guards/role.guard';
 import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [
@@ -12,16 +13,15 @@ const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
+    canActivate: [AuthGuard],
     children: [
       {
         path: 'home',
-        canActivate: [AuthGuard],
         loadChildren: () =>
           import('./modules/home/home.module').then((m) => m.HomeModule),
       },
       {
         path: 'teleconsult',
-        canActivate: [AuthGuard],
         loadChildren: () =>
           import('./modules/teleconsult/teleconsult.module').then(
             (m) => m.TeleconsultModule
@@ -29,7 +29,6 @@ const routes: Routes = [
       },
       {
         path: 'appointments',
-        canActivate: [AuthGuard],
         loadChildren: () =>
           import('./modules/appointments/appointments.module').then(
             (m) => m.AppointmentsModule
@@ -37,7 +36,7 @@ const routes: Routes = [
       },
       {
         path: 'prescriptions',
-        canActivate: [AuthGuard],
+        canActivate: [RoleGuard],
         data: { roles: ['paciente'] },
         loadChildren: () =>
           import('./modules/prescriptions/prescriptions.module').then(
@@ -46,14 +45,14 @@ const routes: Routes = [
       },
       {
         path: 'fuat',
-        canActivate: [AuthGuard],
+        canActivate: [RoleGuard],
         data: { roles: ['doctor'] },
         loadChildren: () =>
           import('./modules/fuat/fuat.module').then((m) => m.FuatModule),
       },
       {
         path: 'reports',
-        canActivate: [AuthGuard],
+        canActivate: [RoleGuard],
         data: { roles: ['doctor'] },
         loadChildren: () =>
           import('./modules/reports/reports.module').then(
@@ -62,19 +61,17 @@ const routes: Routes = [
       },
       {
         path: 'chat',
-        canActivate: [AuthGuard],
         loadChildren: () =>
           import('./modules/chat/chat.module').then((m) => m.ChatModule),
       },
       {
         path: 'forum',
-        canActivate: [AuthGuard],
         loadChildren: () =>
           import('./modules/forum/forum.module').then((m) => m.ForumModule),
       },
       {
         path: 'patients',
-        canActivate: [AuthGuard],
+        canActivate: [RoleGuard],
         data: { roles: ['doctor'] },
         loadChildren: () =>
           import('./modules/patients/patients.module').then(
@@ -83,7 +80,7 @@ const routes: Routes = [
       },
       {
         path: 'questions',
-        canActivate: [AuthGuard],
+        canActivate: [RoleGuard],
         data: { roles: ['paciente'] },
         loadChildren: () =>
           import('./modules/questions/questions.module').then(
--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -10,7 +10,6 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { AuthService } from '../services/auth.service';
 import { SocketService } from '../services/socket.service';
-import { StorageService } from '../services/storage.service';
 
 @Injectable({
   providedIn: 'root',
@@ -31,30 +30,16 @@ export class AuthGuard implements CanActivate {
     | boolean
     | UrlTree {
     if (this.authService.isLoggedIn()) {
-      return this.verifyLoggedIn(route);
+      return this.authService.getLoggedIn().pipe(
+        map((user) => {
+          this.authService.loggedIn = user;
+          this.socketService.connect(user);
+          return true;
+        })
+      );
     }
+    this.authService.loggedIn = null;
     this.router.navigate(['/login']);
     return false;
-  }
-
-  verifyLoggedIn(route: ActivatedRouteSnapshot): Observable<boolean> {
-    return this.authService.getLoggedIn().pipe(
-      map((user) => {
-        if (
-          route.data.roles &&
-          route.data.roles.indexOf(user.roles[0]) === -1
-        ) {
-          this.authService.logout();
-          this.router.navigate(['/login']);
-          return false;
-        }
-        this.authService.loggedIn = user;
-        if (!this.socketService.isConnected()) {
-          console.log('conectado 1 vez');
-          this.socketService.connect(user);
-        }
-        return true;
-      })
-    );
   }
 }
