bd211b9326c0f3693747c2492293cd4c6960fece
Paul Fernando <paulfernando128@gmail.com>
2021-01-24 09:44:40 -0500

    legend and wallpaper

--- src/app/modules/home/pages/home/home.component.ts
+++ src/app/modules/home/pages/home/home.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/core/models/user.model';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  user: User;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private authService: AuthService) {
+    this.user = this.authService.loggedIn;
   }
 
+  ngOnInit(): void {}
 }
